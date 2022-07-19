428bce6c6b19b79a253f97caea837d7d6fdecef9
[ tatiana ] <tatianamcloughlin@gmail.com>
2022-07-19 19:45:44 -0300

    comp8

---
+++ comp8.py
@@ -0,0 +1,11 @@
+#Calcular el número de pulsaciones que una persona debe tener por cada 10 segundos de ejercicio, si la fórmula es: 
+#Número de pulsaciones = (220 - edad)/10
+
+edad= int(input("Ingrese su edad: "))
+
+pulsaciones_correctas = (220 - edad )/10
+
+print (f"Usted deberia de tener {pulsaciones_correctas} pulsaciones por cada 10 segundos de ejercicio.")
+
+
+#resuelto
